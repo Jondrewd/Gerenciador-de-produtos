153ee68f2cd598f4f67a8df08095aa21bdc83d5b
Jondrewd <jonas220703@gmail.com>
2024-04-26 16:49:11 -0300

    update

--- src/main/java/com/jondrewd/course/config/TestConfig.java
+++ src/main/java/com/jondrewd/course/config/TestConfig.java
@@ -1,13 +1,17 @@
 package com.jondrewd.course.config;
 
 import java.util.Arrays;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.jondrewd.course.entities.Order;
 import com.jondrewd.course.entities.User;
+import com.jondrewd.course.entities.enums.OrderStatus;
+import com.jondrewd.course.repositories.OrderRepository;
 import com.jondrewd.course.repositories.UserRepository;
 
 @Configuration
@@ -17,12 +21,20 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria123@gmail.com", "123456", "maria123");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777777", "alex123");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.CANCELED);
+
         userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
 
     
--- src/main/java/com/jondrewd/course/entities/Order.java
+++ src/main/java/com/jondrewd/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.jondrewd.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.jondrewd.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,17 +22,25 @@ public class Order implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+
+    private Integer orderStatus;
     
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);;
     }
 
     public Long getId() {
@@ -56,6 +66,17 @@ public class Order implements Serializable{
     public void setClient(User client) {
         this.client = client;
     }
+    
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+        this.orderStatus = orderStatus.getCode();
+        }
+    }
+
 
     @Override
     public int hashCode() {
