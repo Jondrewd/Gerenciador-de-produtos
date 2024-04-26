6aaf979923558ada44da155c1e0658e82fac1615
Jondrewd <jonas220703@gmail.com>
2024-04-26 16:49:25 -0300

    Create OrderService.java

---
+++ src/main/java/com/jondrewd/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.jondrewd.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jondrewd.course.entities.Order;
+import com.jondrewd.course.repositories.OrderRepository;
+
+@Component
+public class OrderService {
+    
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
