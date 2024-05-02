a28f5c1147e934273fc18cdf2848799582ffe86e
Jondrewd <jonas220703@gmail.com>
2024-05-02 18:02:08 -0300

    Acrescentando o subtotal

--- src/main/java/com/jondrewd/course/entities/Payment.java
+++ src/main/java/com/jondrewd/course/entities/Payment.java
@@ -2,6 +2,8 @@ package com.jondrewd.course.entities;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Payment implements Serializable{
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
--- src/main/java/com/jondrewd/course/services/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jondrewd.course.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jondrewd.course.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-    
-}
